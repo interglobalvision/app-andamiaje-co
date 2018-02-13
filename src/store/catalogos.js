caf816fd5ce9c39fd460da83f1256ebd01648ac0
mikeyrayvon <mikey@interglobal.vision>
2018-02-12 19:31:35 -0600

    update catalogo store shape

--- src/store/catalogos.js
+++ src/store/catalogos.js
@@ -4,11 +4,33 @@ export default {
   activeCatalogo: {
     placeholder: true,
     title: '---- --- -- ------',
+    startDate: '------------',
+    saleDate: '------------',
+    endDate: '------------',
+    lotes: [
+      {
+        title: '-------- -------',
+        artista: {
+          name: '---------- -----------',
+        },
+      },
+    ],
   },
   pastCatalogos: [
     {
       placeholder: true,
       title: '---- --- -- ------',
+      startDate: '------------',
+      saleDate: '------------',
+      endDate: '------------',
+      lotes: [
+        {
+          title: '-------- -------',
+          artista: {
+            name: '---------- -----------',
+          },
+        },
+      ],
     },
   ],
 };
