884382608df1337d17f12752d03fa90aef7956fc
mikeyrayvon <mikey@interglobal.vision>
2018-02-12 19:32:27 -0600

    import lotesReducer to main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import member from './member';
 import recipes from './recipes';
 import noticias from './noticiasReducer';
 import catalogos from './catalogosReducer';
+import lotes from './lotesReducer';
 
 const rehydrated = (state = false, action) => {
   switch (action.type) {
@@ -20,4 +21,5 @@ export default {
   recipes,
   noticias,
   catalogos,
+  lotes,
 };
