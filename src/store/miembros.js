1e03f76016de0918f4788528fa06523b53331869
mikeyrayvon <mikey@interglobal.vision>
2018-02-15 16:05:54 -0600

    add miembros reducer and store

---
+++ src/store/miembros.js
@@ -0,0 +1,20 @@
+export default {
+  loading: true,
+  error: null,
+  miembros: [
+    {
+      placeholder: true,
+      active: false,
+      displayName: '----------',
+      images: [
+        {
+          downloadURL: '-------------------',
+          fullPath: '--------------------',
+          key: '--------------------',
+          name: '--------------------------',
+        }
+      ],
+      collection: [],
+    },
+  ],
+};
