0e99791dcf86bbe83ff943b1af2759bc86cc62cb
mikeyrayvon <mikey@interglobal.vision>
2018-02-20 20:51:05 -0600

    implement selector package for OrderPicker

--- src/native/components/fields/OrderPicker.js
+++ src/native/components/fields/OrderPicker.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Image, View, Text, TouchableOpacity, Picker } from 'react-native';
+import ModalSelector from 'react-native-modal-selector';
 
 class OrderPicker extends Component {
   constructor(props) {
@@ -8,31 +9,21 @@ class OrderPicker extends Component {
   }
 
   render() {
+    let index = 0;
+    const data = [
+      { key: index++, section: true, label: 'Ordenar' },
+      { key: index++, label: 'Artista A-Z', value: 'artist-az' },
+      { key: index++, label: 'Artista Z-A', value: 'artist-za' },
+      { key: index++, label: 'Precio ↑', value: 'price-asc' },
+      { key: index++, label: 'Precio ↓', value: 'price-desc' }
+    ];
+
     return(
-      <Picker
-        onValueChange={this.props.onValueChange}
-        >
-        <Picker.Item
-          label='Ordernar'
-          value='default'
-        />
-        <Picker.Item
-          label='Artista A-Z'
-          value='artist-az'
-        />
-        <Picker.Item
-          label='Artista Z-A'
-          value='artist-za'
-        />
-        <Picker.Item
-          label='Precio ↑'
-          value='price-asc'
-        />
-        <Picker.Item
-          label='Precio ↓'
-          value='price-desc'
-        />
-      </Picker>
+      <ModalSelector
+        data={data}
+        initValue="Ordenar"
+        onChange={(option) => {this.props.onValueChange(option.value)}}
+      />
     )
   }
 
