10fe1142214bcf19e7ce35cee1838f6552947bb4
mikeyrayvon <mikey@interglobal.vision>
2018-02-20 20:04:37 -0600

    add MiembroProfile with wishlist for current user

---
+++ src/native/components/miembros/MiembroProfile.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { ScrollView, View, Image, Dimensions, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import DraftContentRenderer from '../DraftContentRenderer';
+
+import Wishlist from '../wishlist/Wishlist';
+
+import { getResizedImageUrl } from '../../../lib/utilities';
+
+import Spacer from '../Spacer';
+
+const MiembroProfile = ({
+	error,
+	miembros,
+	miembroId,
+  member,
+}) => {
+
+  // Get this miembro from all miembros
+  let miembro = null;
+
+  if (miembroId && miembros) {
+    miembro = miembros.find(item => item.id === miembroId);
+  }
+
+  const placeholder = 'http://via.placeholder.com/100x100';
+
+  const {width, height} = Dimensions.get('window')
+
+  const {
+    displayName,
+    images,
+    collection,
+  } = miembro;
+
+  const imageSrc = images !== undefined ? getResizedImageUrl(images[0], 350, true) : placeholder;
+
+  const renderWishlist = () => {
+    if (miembroId === member.uid) {
+      return (
+        <Wishlist wishlist={member.wishlist} />
+      )
+    }
+    return null;
+  }
+
+	return (
+    <ScrollView>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View>
+          <Spacer />
+          <Image source={{ uri: imageSrc }} style={{ width: 100, height: 100, borderRadius: 50 }} />
+          <Spacer />
+        </View>
+        <View style={{ paddingLeft: 10 }}>
+          <Spacer />
+          { displayName !== 'undefined' ? <Text>{displayName}</Text>  : '' }
+          <Spacer />
+        </View>
+      </View>
+      {renderWishlist()}
+    </ScrollView>
+	);
+};
+
+MiembroProfile.propTypes = {
+	error: PropTypes.string,
+	miembroId: PropTypes.string.isRequired,
+	miembros: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  member: PropTypes.object.isRequired,
+};
+
+MiembroProfile.defaultProps = {
+	error: null,
+};
+
+export default MiembroProfile;
