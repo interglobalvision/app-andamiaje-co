566274b091cff104ff84f7bf97b810b5c67c7763
mikeyrayvon <mikey@interglobal.vision>
2018-02-20 18:02:47 -0600

    add image utility funcs

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -1,10 +1,13 @@
+import { Dimensions } from 'react-native';
+import imageSizes from '../native/constants/imageSizes';
+
 export const getResizedImageUrl = (file, size, square) => {
 
   if(file === undefined) {
     return;
   }
 
-  const name = file.name.replace(/\.[^/.]+$/, '');
+  let name = file.name.replace(/\.[^/.]+$/, '');
 
   let thumbSuffixfix = '_' + size;
 
@@ -14,5 +17,47 @@ export const getResizedImageUrl = (file, size, square) => {
 
   thumbSuffixfix += '_thumb';
 
+  // Encode string
+  name = encodeURIComponent(name);
+  thumbSuffixfix = encodeURIComponent(thumbSuffixfix);
+
   return file.downloadURL.replace(name, name + thumbSuffixfix);
 };
+
+export const getBestImageSize = () => {
+  let bestImageSize = 0;
+  const windowWidth = Dimensions.get('window').width;
+
+  for (i = 1; i < imageSizes.length; i++) {
+    bestImageSize = imageSizes[i];
+
+    if (bestImageSize > windowWidth) {
+      break;
+    }
+  }
+
+  return bestImageSize;
+}
+
+export const getScaledImageDimensions = (imageWidth, imageHeight) => {
+  let imageDimensions = {};
+  const windowWidth = Dimensions.get('window').width;
+
+  if (imageHeight > imageWidth) {
+    const imageStyleWidth = (windowWidth / imageHeight) * imageWidth;
+
+    imageDimensions = {
+      height: windowWidth,
+      width: imageStyleWidth
+    }
+  } else {
+    const imageStyleHeight = (windowWidth / imageWidth) * imageHeight;
+
+    imageDimensions = {
+      width: windowWidth,
+      height: imageStyleHeight
+    }
+  }
+
+  return imageDimensions;
+}
