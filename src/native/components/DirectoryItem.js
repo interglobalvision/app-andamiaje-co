407b68910a1e808e26fd05e858a812f21d9e17bd
mikeyrayvon <mikey@interglobal.vision>
2018-02-22 17:42:18 -0600

    combine ArtistasListItem MiembrosListItem  as DirectoryItem

--- src/native/components/DirectoryItem.js
+++ src/native/components/DirectoryItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { TouchableOpacity, View, ActivityIndicator, Image, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { getResizedImageUrl } from '../../../lib/utilities';
-import styles from '../../constants/styles';
+import { getResizedImageUrl } from '../../lib/utilities';
+import styles from '../constants/styles';
 
-const ArtistasListItem = ({ name, images, id, type }) => {
+const DirectoryItem = ({ name, images, id, type }) => {
   const placeholder = 'http://via.placeholder.com/50x50';
   // TODO: Need a placeholder image for missing avatars
   const imageSrc = images !== undefined ? getResizedImageUrl(images[0], 350, true) : placeholder;
 
-  const onPress = id => Actions.artista({ match: { params: { id: String(id) } } });
+  let onPress = id => Actions.miembro({ match: { params: { id: String(id) } } });
+
+  if (type === 'artista') {
+    onPress = id => Actions.artista({ match: { params: { id: String(id) } } });
+  }
 
   return (
     <TouchableOpacity onPress={() => onPress(id)} style={[
@@ -34,4 +38,4 @@ const ArtistasListItem = ({ name, images, id, type }) => {
   )
 };
 
-export default ArtistasListItem;
+export default DirectoryItem;
