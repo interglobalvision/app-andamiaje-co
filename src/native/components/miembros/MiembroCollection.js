990eb694f8dea5bdab8c8d964099b3595599e749
mikeyrayvon <mikey@interglobal.vision>
2018-02-22 13:38:55 -0600

    assign id comparison to const

--- src/native/components/miembros/MiembroCollection.js
+++ src/native/components/miembros/MiembroCollection.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import SectionHeader from '../SectionHeader';
 import Spacer from '../Spacer';
 
 const MiembroCollection = ({ miembroId, memberId, collection }) => {
-  const emptyNotice = miembroId === memberId ? 'Tu colección aún está vacía' : 'Esta colección aún está vacía';
+  const isCurrentMember = miembroId === memberId ? true : false;
+  const headerTitle = isCurrentMember ? 'Tu colección' : 'Colección';
+  const emptyNotice = isCurrentMember ? 'Tu colección aún está vacía' : 'Esta colección aún está vacía';
 
   return (
     <View>
+      <SectionHeader title={headerTitle}/>
       <Spacer />
       <View>
         <Text>{emptyNotice}</Text>
