11d6d613e4bfc675af995461f2250cc847fe3b89
mikeyrayvon <mikey@interglobal.vision>
2018-02-22 17:45:12 -0600

    rename DirectoryListItem

--- src/native/components/DirectoryListItem.js
+++ src/native/components/DirectoryListItem.js
@@ -4,7 +4,7 @@ import { Actions } from 'react-native-router-flux';
 import { getResizedImageUrl } from '../../lib/utilities';
 import styles from '../constants/styles';
 
-const DirectoryItem = ({ name, images, id, type }) => {
+const DirectoryListItem = ({ name, images, id, type }) => {
   const placeholder = 'http://via.placeholder.com/50x50';
   // TODO: Need a placeholder image for missing avatars
   const imageSrc = images !== undefined ? getResizedImageUrl(images[0], 350, true) : placeholder;
@@ -38,4 +38,4 @@ const DirectoryItem = ({ name, images, id, type }) => {
   )
 };
 
-export default DirectoryItem;
+export default DirectoryListItem;
