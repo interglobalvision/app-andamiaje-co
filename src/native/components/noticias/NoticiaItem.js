022357f6e67a4cbb8dc9539a1ce47b8bc1542bcf
Cas <cas@interglobal.vision>
2018-02-22 17:57:15 -0600

    add spanish date locale

--- src/native/components/noticias/NoticiaItem.js
+++ src/native/components/noticias/NoticiaItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Image, View, TouchableOpacity, Text } from 'react-native';
 import { distanceInWordsToNow } from 'date-fns';
+import es from 'date-fns/locale/es';
 import getRNDraftJSBlocks from 'react-native-draftjs-render';
 import { Actions } from 'react-native-router-flux';
 import styles from '../../constants/styles';
@@ -35,7 +36,7 @@ const NoticiaItem = ({item}) => {
       <View style={styles.paddingBottomBasic}>
         <Text>
           <Text style={[styles.fontBold, styles.fontSizeMid ]}>{item.title}</Text>          <TextBullet />
-          <Text>{distanceInWordsToNow(item.publishDate)}</Text>
+          <Text>{distanceInWordsToNow(item.publishDate, { locale: es })}</Text>
         </Text>
       </View>
 
