7cd5213353de179581b8f6dacb36855f4cd47aae
mikeyrayvon <mikey@interglobal.vision>
2018-02-22 19:11:40 -0600

    revise boilerplate draftjs styles

--- src/native/components/DraftContentRenderer.js
+++ src/native/components/DraftContentRenderer.js
@@ -1,77 +1,23 @@
 import React from 'react';
 import getRNDraftJSBlocks from 'react-native-draftjs-render';
 import { View, StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 16,
-    backgroundColor: '#f4f4f4',
-  },
-});
+import colors from '../constants/colors';
+import styleConstants from '../constants/styleConstants';
+import styles from '../constants/styles';
 
 const customStyles = StyleSheet.flatten({
   unstyled: {
-    fontSize: 18,
-    fontWeight: 'normal',
-    letterSpacing: -0.75,
-    lineHeight: 32,
-    marginBottom: 21,
+    fontSize: styleConstants.fontSizeBasic,
+    marginBottom: styleConstants.paddingSmall,
+    lineHeight: styleConstants.lineHeightParagraph,
   },
   link: {
-    color: '#c4170c',
-    fontWeight: 'bold',
-    textDecorationLine: 'none',
+    color: colors.darkGrey,
+    fontWeight: styleConstants.fontWeightBold,
   },
-  unorderedListItemContainer: {
-    marginBottom: 16,
-    position: 'relative',
-  },
-  unorderedListItemBullet: {
-    marginRight: 18,
-    position: 'relative',
-    top: 14,
-    width: 6,
-    height: 6,
-    alignSelf: 'flex-start',
-  },
-  'unordered-list-item': {
-    fontSize: 18,
-    lineHeight: 32,
-    alignSelf: 'flex-start',
-    flex: 1,
-  },
-    orderedListContainer: {
-      marginBottom: 16,
-    },
-    orderedListItemNumber: {
-      fontSize: 18,
-      lineHeight: 32,
-      marginRight: 11,
-      alignSelf: 'flex-start',
-      color: '#c4170c',
-    },
-  'ordered-list-item': {
-    alignSelf: 'flex-start',
-    fontSize: 18,
-    lineHeight: 32,
-    flex: 1,
-  },
-  'code-block': {
-    backgroundColor: '#e2e2e2',
-  },
-  blockquote: {
-    fontWeight: 'bold',
-    color: '#333',
-    lineHeight: 33,
-    paddingTop: 24,
-    marginBottom: 24,
-    fontSize: 33,
-    letterSpacing: -2,
-  },
-  viewAfterList: {
-    marginBottom: 32,
-  },
+  paragraph: {
+    paddingBottom: styleConstants.paddingLarge,
+  }
 });
 
 const DraftContentRenderer = ({rawContent}) => {
@@ -85,7 +31,12 @@ const DraftContentRenderer = ({rawContent}) => {
   const blocks = getRNDraftJSBlocks(params);
 
   return (
-    <View style={styles.container}>
+    <View style={[
+      styles.container,
+      styles.paddingTopBasic,
+      styles.paddingBottomSmall,
+      styles.bordered
+    ]}>
     {blocks}
     </View>
   );
