cb3ba6fc0ddddb3a6466d1df8a863f7a1592a5e4
mikeyrayvon <mikey@interglobal.vision>
2018-02-22 19:11:11 -0600

    move style constants

---
+++ src/native/constants/styleConstants.js
@@ -0,0 +1,36 @@
+// Font size
+const fontSizeBasic = 14;
+const fontSizeSmall = 10;
+const fontSizeMid = 21;
+const fontSizeLarge = 28;
+
+// Font weight
+const fontWeightBold = '800';
+
+// Line height
+const lineHeightBasic = 18;
+const lineHeightParagraph = 21;
+
+// Padding
+const paddingBasic = 20;
+const paddingSmall = paddingBasic / 2; // 10
+const paddingMid = paddingBasic * 1.5; // 30
+const paddingLarge = paddingBasic * 2; // 40
+
+export default styleConstants = {
+  // Font weight
+  fontWeightBold: fontWeightBold,
+  // Line height
+  lineHeightBasic: lineHeightBasic,
+  lineHeightParagraph: lineHeightParagraph,
+  // Font size
+  fontSizeBasic: fontSizeBasic,
+  fontSizeSmall: fontSizeSmall,
+  fontSizeMid: fontSizeMid,
+  fontSizeLarge: fontSizeLarge,
+  // Padding
+  paddingBasic: paddingBasic,
+  paddingSmall: paddingSmall,
+  paddingMid: paddingMid,
+  paddingLarge: paddingLarge,
+}
