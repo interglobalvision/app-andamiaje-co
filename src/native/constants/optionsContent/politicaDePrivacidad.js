e3fe792f29f071d304cde36164cf202641a0540b
mikeyrayvon <mikey@interglobal.vision>
2018-02-24 17:59:45 -0600

    add linebreaks

--- src/native/constants/optionsContent/politicaDePrivacidad.js
+++ src/native/constants/optionsContent/politicaDePrivacidad.js
@@ -1,12 +1,16 @@
-export default `POLÍTICA DE PRIVACIDAD
-El presente Política de Privacidad establece los términos en que Andamiaje.co usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su aplicación. Esta aplicación está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento.
-Información que es recogida
+export default `El presente Política de Privacidad establece los términos en que Andamiaje.co usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su aplicación. Esta aplicación está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento.
+
+Información que es recogida:
 Nuestra aplicación podrá recoger información personal como: Nombre e información de contacto (como su dirección de correo electrónico). Así mismo cuando sea necesario podrá ser requerida información específica para procesar algún pedido o realizar una entrega o facturación.
-Uso de la información recogida
+
+Uso de la información recogida:
 Nuestra aplicación emplea la información con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios y mejorar nuestros productos y servicios.  Es posible que sean enviados correos electrónicos periódicamente a través de nuestro sitio, estos correos electrónicos serán enviados a la dirección que usted proporcione y podrán ser cancelados en cualquier momento.
+
 Andamiaje.co está altamente comprometido para cumplir con el compromiso de mantener su información segura. Usamos los sistemas más avanzados y los actualizamos constantemente para asegurarnos que no exista ningún acceso no autorizado.
-Enlaces a Terceros
+
+Enlaces a Terceros:
 Esta aplicación pudiera contener enlaces a otros sitios que pudieran ser de su interés. Una vez que usted de clic en estos enlaces y abandone nuestra aplicación, ya no tenemos control sobre al sitio al que es redirigido y por lo tanto no somos responsables de los términos o privacidad ni de la protección de sus datos en esos otros sitios terceros. Dichos sitios están sujetos a sus propias políticas de privacidad por lo cual es recomendable que los consulte para confirmar que usted está de acuerdo con estas.
-Control de su información personal
+
+Control de su información personal:
 Esta compañía no venderá, cederá ni distribuirá la información personal que es recopilada sin su consentimiento, salvo que sea requerido por un juez con un orden judicial.
 `;
