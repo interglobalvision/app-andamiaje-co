cdc1a69a6205ecdbebdfd4861b137d52e7fde187
mikeyrayvon <mikey@interglobal.vision>
2018-02-26 15:25:59 -0600

    move current member to top of list

--- src/native/components/miembros/MiembrosList.js
+++ src/native/components/miembros/MiembrosList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, StyleSheet, FlatList, TouchableOpacity, RefreshControl } from 'react-native';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
 import { Container, Content } from 'native-base';
 import { Actions } from 'react-native-router-flux';
+import _pull from 'lodash/pull';
+import _clone from 'lodash/clone';
 import styles from '../../constants/styles';
 
 import Loading from '../Loading';
@@ -13,6 +15,7 @@ const MiembrosList = ({
   error,
   loading,
   miembros,
+  member,
   reFetch,
 }) => {
   // Loading
@@ -25,22 +28,25 @@ const MiembrosList = ({
 
   const onPress = item => Actions.miembro({ match: { params: { id: String(item.id) } } });
 
+  const currentMember = miembros.find(miembro => miembro.id === member.uid);
+
+  let displayCurrentMember = false;
+
+  let miembrosArray = _clone(miembros);
+
+  if (currentMember !== undefined && currentMember !== null) {
+    displayCurrentMember = true;
+    _pull(miembrosArray, currentMember);
+  }
+
   return (
     <ScrollView style={styles.backgroundWhite}>
-      <FlatList
-        numColumns={1}
-        data={miembros}
-        renderItem={({ item }) => (
-          <DirectoryListItem id={item.id} name={item.displayName} images={item.images} type={'miembro'} />
-        )}
-        keyExtractor={keyExtractor}
-        refreshControl={
-          <RefreshControl
-            refreshing={loading}
-            onRefresh={reFetch}
-          />
+      <View>
+        { displayCurrentMember &&
+          <DirectoryListItem key={currentMember.key} id={currentMember.id} name={currentMember.displayName} images={currentMember.images} type={'miembro'} />
         }
-      />
+        { miembrosArray.map( (item, key) => <DirectoryListItem key={key} id={item.id} name={item.displayName} images={item.images} type={'miembro'} />)}
+      </View>
     </ScrollView>
   );
 };
@@ -49,6 +55,7 @@ MiembrosList.propTypes = {
   error: PropTypes.string,
   loading: PropTypes.bool.isRequired,
   miembros: PropTypes.array.isRequired,
+  member: PropTypes.object.isRequired,
   reFetch: PropTypes.func,
 };
 
