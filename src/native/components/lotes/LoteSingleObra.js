63924b872ac2108fc58ffeb8771ea03ec3233b2c
mikeyrayvon <mikey@interglobal.vision>
2018-02-27 14:40:10 -0600

    add obra notes

--- src/native/components/lotes/LoteSingleObra.js
+++ src/native/components/lotes/LoteSingleObra.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-
-import Spacer from '../Spacer';
+import DraftContentRenderer from '../DraftContentRenderer';
 
 import CarouselHolder from '../carousel/CarouselHolder';
 
@@ -12,12 +11,20 @@ import styles from '../../constants/styles';
 const LoteSingleObra = ({
   obra, border
 }) => {
+  const {
+    artista,
+    title,
+    year,
+    materials,
+    dimensions,
+    notesRawContent,
+  } = obra;
 
   const onPress = () => Actions.artista({ match: { params: { id: String(obra.artista.id) } } });
 
-  let holderStyle = [styles.paddingBottomLarge, styles.bordered];
-  if (!border) {
-    holderStyle = [styles.paddingBottomLarge];
+  let holderStyle = [styles.paddingBottomLarge];
+  if (border) {
+    holderStyle.pust(styles.bordered);
   }
 
   return (
@@ -27,18 +34,21 @@ const LoteSingleObra = ({
         <TouchableOpacity
           onPress={onPress}
           style={[
-            styles.paddingBottomBasic,
+            styles.paddingBottomSmall,
             styles.flexRow,
             { justifyContent: 'space-between' }
           ]}
         >
-          <Text style={styles.fontBold}>{obra.artista.name}</Text>
-          <Text style={[styles.textLink]}>Ver bio</Text>
+          <Text style={styles.fontBold}>{artista.name}</Text>
+          <Text style={[styles.textLink, styles.fontSizeSmall]}>Ver bio</Text>
         </TouchableOpacity>
         <View>
-          <Text><Text style={[styles.fontItalic]}>{obra.title}</Text>, {obra.year}</Text>
-          <Text>{obra.materials}</Text>
-          <Text>{obra.dimensions}</Text>
+          <Text><Text style={[styles.fontItalic]}>{title}</Text>, {year}</Text>
+          <Text>{materials}</Text>
+          <Text>{dimensions}</Text>
+        </View>
+        <View style={[styles.paddingTopBasic]}>
+          <DraftContentRenderer rawContent={notesRawContent} />
         </View>
       </View>
     </View>
