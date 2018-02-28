7d3cc43d31e2001b1898830ea234da81259f678a
mikeyrayvon <mikey@interglobal.vision>
2018-02-27 20:23:01 -0600

    add artista video component

---
+++ src/native/components/artistas/ArtistaVideo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text } from 'react-native';
+import { List } from 'native-base';
+import { Actions } from 'react-native-router-flux';
+import styles, { containerWidth } from '../../constants/styles';
+import SectionHeader from '../SectionHeader';
+import Thumbnail from 'react-native-thumbnail-video';
+
+const ArtistaVideo = ({
+  video,
+}) => {
+  const hasVideo = video.url !== undefined && video.url !== '' && video.provider === 'youtube';
+
+  return (
+    <View>
+      <SectionHeader title={'Video'} />
+      <View style={[
+        styles.container,
+        styles.paddingTopSmall,
+        styles.paddingBottomLarge,
+      ]}>
+        <Thumbnail
+          url={video.url}
+          imageWidth={containerWidth}
+          imageHeight={((containerWidth / 16) * 9)}
+          iconStyle={{width: 25, height: 29}}
+        />
+      </View>
+    </View>
+  );
+};
+
+ArtistaVideo.propTypes = {
+  video: PropTypes.object.isRequired,
+};
+
+export default ArtistaVideo;
