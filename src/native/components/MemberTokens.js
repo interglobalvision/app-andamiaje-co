8932d247ec5f5bae1d40c8e80e2eb03627fcd381
mikeyrayvon <mikey@interglobal.vision>
2018-03-01 20:47:15 -0600

    import connect

--- src/native/components/MemberTokens.js
+++ src/native/components/MemberTokens.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 import styles from '../constants/styles';
 
-const MiembroProfile = ({
+const MemberTokens = ({
 	miembros,
   member,
 }) => {
