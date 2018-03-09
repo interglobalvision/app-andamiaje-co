b4b0a2ab1b9fb37118871d82faa64b1bb9e31a1a
mikeyrayvon <mikey@interglobal.vision>
2018-03-08 19:34:24 -0600

    move bio to component. hide & animate show

---
+++ src/native/components/artistas/ArtistaBio.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dimensions, TouchableOpacity, View, Text } from 'react-native';
+import styles from '../../constants/styles';
+import * as Animatable from 'react-native-animatable';
+import { LinearGradient } from 'expo';
+import DraftContentRenderer from '../DraftContentRenderer';
+
+class ArtistaBio extends React.Component {
+  static propTypes = {
+    bioRawContent: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    bioRawContent: '',
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  showFullBio = () => {
+    this.containerView.transitionTo({
+        height: this.bioHeight,
+      },
+      1000,
+      'linear'
+    );
+
+    this.buttonView.transitionTo({
+        opacity: 0,
+      },
+      1000,
+      'linear'
+    );
+  }
+
+  render() {
+    const { bioRawContent } = this.props;
+
+    const windowHeight = Dimensions.get('window').height;
+
+    const handleViewRef = ref => this.view = ref;
+
+    if (bioRawContent === '' || bioRawContent === undefined) {
+      return null
+    }
+    
+    return (
+      <View style={[
+        styles.container,
+        styles.paddingTopBasic,
+        styles.paddingBottomBasic,
+        styles.bordered,
+      ]}>
+        <Animatable.View
+          ref={ref => this.containerView = ref}
+          style={[
+            {
+              overflow: 'hidden',
+              height: windowHeight * .3,
+            }
+          ]}
+        >
+          <View
+            onLayout={(event) => {
+              this.bioHeight = event.nativeEvent.layout.height;
+            }}
+          >
+            <DraftContentRenderer rawContent={bioRawContent} />
+          </View>
+          <Animatable.View
+            ref={ref => this.buttonView = ref}
+            style={[
+              {
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                justifyContent: 'flex-end',
+              }
+          ]}>
+            <LinearGradient
+              colors={[
+                'rgba(255,255,255,0)',
+                'rgba(255,255,255,1)',
+                'rgba(255,255,255,1)'
+              ]}
+            >
+              <TouchableOpacity
+                onPress={this.showFullBio}
+                style={[
+                  styles.paddingTopLarge,
+                  styles.paddingBottomSmall,
+                  {
+                    backgroundColor: 'transparent',
+                  }
+              ]}>
+                <Text style={[
+                  styles.textLink,
+                ]}>Ver todo</Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </Animatable.View>
+        </Animatable.View>
+      </View>
+    );
+  }
+};
+
+export default ArtistaBio;
