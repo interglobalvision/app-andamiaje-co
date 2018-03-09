1c67aea4bcbbca3fc76c3f9c3aab191f04e5a304
mikeyrayvon <mikey@interglobal.vision>
2018-03-08 20:13:08 -0600

    add notes to portfolio item

--- src/native/components/artistas/PortfolioItem.js
+++ src/native/components/artistas/PortfolioItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Image, View, Text } from 'react-native';
 import { List } from 'native-base';
 import { Actions } from 'react-native-router-flux';
+import Hyperlink from 'react-native-hyperlink';
 
 import styles from '../../constants/styles';
 
@@ -12,6 +13,7 @@ const PortfolioItem = ({
   item,
   name,
 }) => {
+  console.log(item);
 
   return (
     <View style={[styles.bordered, styles.paddingTopBasic, styles.paddingBottomLarge]}>
@@ -25,6 +27,15 @@ const PortfolioItem = ({
           <Text style={[styles.lineHeightBasic]}>{item.materials}</Text>
           <Text style={[styles.lineHeightBasic]}>{item.dimensions}</Text>
         </View>
+        {(item.notes !== undefined && item.notes !== '') &&
+          <View style={[
+            styles.paddingTopBasic,
+          ]}>
+            <Hyperlink linkDefault={true} linkStyle={styles.textLink}>
+              <Text style={[styles.lineHeightBasic]}>{item.notes}</Text>
+            </Hyperlink>
+          </View>
+        }
       </View>
     </View>
   );
