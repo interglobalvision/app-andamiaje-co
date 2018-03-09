f0d24af62cb25f4d87fbc6eca45d7b0a071d8f6d
mikeyrayvon <mikey@interglobal.vision>
2018-03-09 00:38:39 -0600

    add error case for too-expensive

--- src/native/components/BuyButton.js
+++ src/native/components/BuyButton.js
@@ -110,6 +110,9 @@ class BuyButton extends React.Component {
                   showNotification('Estas obras ya tienen dueño');
                 }
                 break;
+              case 'lote/too-expensive':
+                showNotification('No tienes suficientes tokens');
+                break;
               case 'unauthorized':
                 showNotification('No puedes realizar esta acción');
                 break;
