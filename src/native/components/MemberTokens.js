85bca8eea3fadbe31944a7d28e689517d66750c0
mikeyrayvon <mikey@interglobal.vision>
2018-03-14 15:16:00 -0600

    bigger member tokens in header

--- src/native/components/MemberTokens.js
+++ src/native/components/MemberTokens.js
@@ -28,7 +28,6 @@ const MemberTokens = ({
         styles.container,
       ]}>
         <Text style={[
-          styles.fontSizeSmall,
           styles.fontFamilyMedium,
           {
             letterSpacing: 1
