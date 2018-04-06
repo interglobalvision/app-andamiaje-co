c9d4a398974374502fd2466957929a6f4f9c95f0
Cas <cas@interglobal.vision>
2018-04-06 16:19:25 -0500

    fix cloud function url for production

--- src/constants/functions.js
+++ src/constants/functions.js
@@ -1,1 +1,9 @@
-export const CloudFunctionsUrl = 'https://us-central1-igv-andamiaje-co.cloudfunctions.net';
+// Cloud functions url for dev
+let FunctionsUrl = 'https://us-central1-igv-andamiaje-co.cloudfunctions.net';
+
+// Cloud functions url for prod
+if (Expo.Constants.manifest.releaseChannel === 'prod-v1') {
+  FunctionsUrl = 'https://us-central1-prod-andamiaje-co.cloudfunctions.net'
+}
+
+export const CloudFunctionsUrl = FunctionsUrl;
