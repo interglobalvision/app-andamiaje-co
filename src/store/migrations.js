baf22c53954a2852e7df4e990ba4009e02e3c304
Cas <cas@interglobal.vision>
2018-04-17 12:59:52 -0500

    add migrations

---
+++ src/store/migrations.js
@@ -0,0 +1,13 @@
+export const migrations = {
+  1: ({_persist}) => {
+    return {_persist}; // reset all state, except version
+  },
+  2: (state) => {
+    return {
+      ...state,
+      catalogos: undefined,
+      countdown: undefined,
+      calendar: undefined,
+    }; // reset all state, except version
+  }
+}
